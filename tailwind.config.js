16e91063da632a30e19933fa41d8e2730998eb20
MortenMunk <mortenmunk97@gmail.com>
2023-01-31 14:12:46 +0100

    Added Lato font to navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    fontFamily: {
+      'lato': ['Lato', 'sans-serif'],
+    },
     extend: {
       colors: {
         'light-navy': '#567189',
