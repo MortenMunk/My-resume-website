90f7b1c3036cdf391a05791fe94f58d947fffcbc
MortenMunk <mortenmunk97@gmail.com>
2023-01-31 14:20:04 +0100

    changed text size

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='sticky top-0 h-20 w-screen bg-light-navy shadow-lg'>
-        <ul className='flex justify-center space-x-28 h-full items-center text-white shrink font-lato'>
+        <ul className='flex justify-center space-x-28 h-full items-center text-white shrink font-lato text-xl'>
             <li>About</li>
             <li>Experience</li>
             <li>Projects</li>
