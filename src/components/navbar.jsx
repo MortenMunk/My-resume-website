995204296f4390ec500b4f3aec853c6d85d87433
MortenMunk <mortenmunk97@gmail.com>
2023-02-06 11:45:07 +0100

    navbar update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
     <div className='sticky top-0 h-20 w-screen bg-light-navy shadow-lg'>
-        <ul className='flex justify-center space-x-28 h-full items-center text-white shrink font-lato text-xl'>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Projects</li>
-            <li>Contact</li>
-        </ul>
+      <ul className='hidden md:flex justify-center space-x-28 h-full items-center text-white shrink font-lato text-xl uppercase tracking-wider'>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div className='md:hidden flex justify-end items-center h-full mx-5 text-white'>
+        <FaBars className='text-2xl' />
+      </div>
+
+      {/* Mobile Menu */}
+      <ul className='hidden'>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
     </div>
   )
 }
